1ba1b38bce7767902f3887ce4ed87026da996346
ANKIT KUMAR <ankityadav12044@gmail.com>
2025-07-18 16:17:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+
+    (function () {
+        var scroll = new LocomotiveScroll();
+    })();
+
+function come(){
+  document.getElementById('image1').style.display='block';
+  document.getElementById('image1').style.marginTop='-278px';
+   let img=   document.getElementById('image1');
+    img.src="Sundown Studio/im.webp";
+  
+}
+
+function out(){
+    document.getElementById('image1').style.display='none';
+
+}
+
+function come2(){
+  document.getElementById('image1').style.display='block';
+  document.getElementById('image1').style.marginTop='-278px';
+  let img=   document.getElementById('image1');
+    img.src="Sundown Studio/img1.webp";
+   
+}
+
+function out2(){
+ document.getElementById('image1').style.display='none';
+}
+
+
+
+function come3(){
+  document.getElementById('image1').style.display='block';
+  document.getElementById('image1').style.marginTop='-278px';
+   let img=   document.getElementById('image1');
+    img.src="Sundown Studio/imgs.webp";
+}
+
+function out3(){
+ document.getElementById('image1').style.display='none';
+}
+
+
+function imag1(){
+ let x =  document.getElementById('gallery');
+  x.src="sundown studio/imageX.webp";
+document.getElementById('para').innerText="We’re with you every step of the way, from the project initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience.";
+}
+
+function image(){
+    
+  let x =  document.getElementById('gallery');
+  x.src="sundown studio/img1.webp";
+    document.getElementById('para').innerText= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem impedit consequuntur dolorem, odio doloremque voluptate et in facere soluta ullam reiciendis sunt illum autem iste magni odit repellendus! ";
+
+}
+
+function imag(){
+  let y =  document.getElementById('gallery');
+  y.src="sundown studio/images.webp";
+  document.getElementById('para').innerText=" consectetur adipisicing elit. Rem impedit from the project initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience.";
+}
+
